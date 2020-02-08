5336d331427f620ba9439ebf236453eae3ff8c04
TheVenYear <47949509+TheVenYear@users.noreply.github.com>
2020-02-08 08:58:47 +0300

    Добавьте файлы проекта.

---
+++ 2s1lab.cpp
@@ -0,0 +1,43 @@
+﻿#include "iostream"
+
+struct Vector {
+	const int ArrayRaiser = 50;
+	int Count = 0;
+	int ArrayCounter = ArrayRaiser;
+	int *Array = new int[ArrayCounter];
+
+	void Add(int item) {
+		if (Count < ArrayCounter) {
+			Array[Count] = item;
+			Count++;
+		}
+		else {
+			int* TempArray = new int[ArrayCounter + ArrayRaiser];
+			for (int i = 0; i < Count; i++) {
+				TempArray[i] = Array[i];
+			}
+			TempArray[Count] = item;
+			ArrayCounter += ArrayRaiser;
+			delete[] Array;
+			Array = TempArray;
+			Count++;
+		}
+	}
+
+	int Index(int index) {
+		return Array[index];
+	}
+};
+int main(){
+	Vector vector;
+	for (int i = 0; i < 100; i++)
+	{
+		vector.Add(i);
+	}
+
+	for (int i = 0; i < vector.Count; i++)
+	{
+		printf("%i\n", vector.Index(i));
+	}
+	printf("\nCounter - %i", vector.Count);
+}
